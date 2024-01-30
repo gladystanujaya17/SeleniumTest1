bdf502a7c2dca5393d7986a3fa733b5904728f6e
gladystanujaya17 <gladystana17@gmail.com>
2024-01-30 21:05:39 +0700

    Eight commit

--- SeleniumTest1/src/com/Gladys/CheckBoxRadioButtons.java
+++ SeleniumTest1/src/com/Gladys/CheckBoxRadioButtons.java
@@ -14,15 +14,39 @@ public class CheckBoxRadioButtons {
 		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\dell\\\\Downloads\\\\chromedriver-win64\\\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();	
-		
 		driver.get("https://the-internet.herokuapp.com/checkboxes");
+		
+		System.out.println("***CHECKBOX***");
+		
+		// Checkbox 1
 		WebElement checkBox1 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
 		System.out.println("Is checkBox1 displayed: " + checkBox1.isDisplayed());
 		System.out.println("Is checkBox1 enabled: " + checkBox1.isEnabled());
-		System.out.println("Is checkBox1 selected: " + checkBox1.isSelected());
-		checkBox1.click();
-		System.out.println("Is checkBox1 selected after clicked: " + checkBox1.isSelected());
+		System.out.println("Is checkBox1 selected: " + checkBox1.isSelected()); //false
+		checkBox1.click(); //select
+		System.out.println("Is checkBox1 selected after clicked: " + checkBox1.isSelected()); //true
+		
+		System.out.println("-----");
+		
+		// Checkbox 2
+		WebElement checkBox2 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]"));
+		System.out.println("Is checkBox1 displayed: " + checkBox2.isDisplayed());
+		System.out.println("Is checkBox1 enabled: " + checkBox2.isEnabled());
+		System.out.println("Is checkBox1 selected: " + checkBox2.isSelected()); //true
+		checkBox2.click(); //deselect
+		System.out.println("Is checkBox1 selected after clicked: " + checkBox2.isSelected()); //false
 
+		// Radio Button
+		System.out.println("***RADIO BUTTONS***");
+
+		driver.navigate().to("https://demo.guru99.com/test/radio.html");
+		
+		WebElement radioButtonOpt2 = driver.findElement(By.xpath("//*[@id=\"vfb-7-2\"]"));
+		System.out.println("Is option 2 displayed: " + radioButtonOpt2.isDisplayed());
+		System.out.println("Is option 2 enabled: " + radioButtonOpt2.isEnabled());
+		System.out.println("Is option 2 selected: " + radioButtonOpt2.isSelected()); // false
+		radioButtonOpt2.click(); // select
+		System.out.println("Is option 2 selected after clicked: " + radioButtonOpt2.isSelected()); // true
 	}
 
 }
---
+++ SeleniumTest1/src/com/Gladys/HandleDropDownJava.java
@@ -0,0 +1,47 @@
+package com.Gladys;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class HandleDropDownJava {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		// #14 Handling Dropdown
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\dell\\\\Downloads\\\\chromedriver-win64\\\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();	
+		driver.get("https://the-internet.herokuapp.com/dropdown");
+		
+		// Dropdown
+		WebElement dropDown = driver.findElement(By.xpath("//*[@id=\"dropdown\"]"));
+		Select dropDownSelect = new Select(dropDown);
+		
+		// Select by index
+		dropDownSelect.selectByIndex(2);
+		
+		Thread.sleep(2000);
+		
+		// Select by value
+		dropDownSelect.selectByValue("1");
+		
+		Thread.sleep(2000);
+		
+		// Select by visible text
+		dropDownSelect.selectByVisibleText("Option 2");
+		
+		// Get Options
+		List<WebElement> listOptions = dropDownSelect.getOptions();
+		
+		for(WebElement option: listOptions) {
+			System.out.println(option.getText());
+		}
+	}
+
+}
